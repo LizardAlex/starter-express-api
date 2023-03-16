05bb500f5c54eb4c0281c6629c36e1ac0248d905
LizardAlex <alexandr-91@bk.ru>
2023-03-16 19:07:15 +0300

    u

--- index.js
+++ index.js
@@ -1,37 +1,34 @@
+const CyclicDB = require('cyclic-dynamodb');
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const CyclicDB = require('cyclic-dynamodb');
-const db = CyclicDB('vast-plum-badger-suitCyclicDB');
+const db = CyclicDB(process.env.CYCLICDB_APP_ID);
+const animalCollection = db.collection('game_events');
 
 app.use(express.json());
 app.use(express.static('public'));
 
 app.post("/", async (req, res) => {
   const { user_id, game_name, event_name, event_value } = req.body;
-  try {
-    const animalCollection = db.collection("game_events");
-    await animalCollection.set(user_id, {
-      game_name: game_name,
-      event_name: event_name,
-      event_value: event_value,
-      created_at: new Date().toISOString(),
-    });
-    res.sendStatus(200);
-  } catch (err) {
-    console.error(err);
-    res.sendStatus(500);
-  }
+  const data = {
+    user_id,
+    game_name,
+    event_name,
+    event_value,
+    created_at: new Date().toISOString()
+  };
+  await animalCollection.set(user_id, data);
+  res.sendStatus(200);
 });
 
 app.get("/gameData", async (req, res) => {
-  const animalCollection = db.collection('game_events');
   try {
-    const items = await animalCollection.find({});
-    res.json(items);
-  } catch (err) {
-    console.error(err);
+    const items = await animalCollection.list();
+    const data = items.map(({key, value}) => ({user_id: key, ...value}));
+    res.json(data);
+  } catch (error) {
+    console.error(error);
     res.sendStatus(500);
   }
 });
